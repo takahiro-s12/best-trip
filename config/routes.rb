108f4df822744752c14770ee34be04bcb2da1df8
takahiro1202 <tk.tennis.w@gmail.com>
2021-03-09 22:57:31 +0900

    [Add]user-edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about'
-  resource :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
